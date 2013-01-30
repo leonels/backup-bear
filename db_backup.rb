589f48629bae50233df5b16e3fe64fcae4ff77b6
Leonel Santos <leonelsantos.me@gmail.com>
2013-01-30 10:44:13 -0600

    Customize banner

--- db_backup.rb
+++ db_backup.rb
@@ -5,17 +5,21 @@ require 'optparse'
 
 options = {}
 option_parser = OptionParser.new do |opts|
+
+  executable_name = File.basename($PROGRAM_NAME)
+  opts.banner = "Usage: #{executable_name} [options] database_name"
+
 	# Create a switch
-	opts.on("-i", "--iteration") do
+	opts.on("-i", "--iteration", 'Indicate that this backup is an "iteration" backup') do
 		options[:iteration] = true
 	end
 
 	# Create a flag
-	opts.on("-u USER", /^.+\..+$/) do |user|
+	opts.on("-u USER", /^.+\..+$/, 'Database username, in first.last format') do |user|
 		options[:user] = user
 	end
 
-	opts.on("-p PASSWORD") do |password|
+	opts.on("-p PASSWORD", 'Database password') do |password|
 		options[:password] = password
 	end
 
